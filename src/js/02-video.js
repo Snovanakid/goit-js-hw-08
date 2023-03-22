23b80ecce6a5192a9b3ffc182547cb7ac5c69d64
Snovanakid <denchapro@gmail.com>
2023-03-22 14:26:25 +0200

    Updated 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,12 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-    
+
 const currentTimeString = localStorage.getItem('videoplayer-current-time');
 
-player.setCurrentTime(JSON.parse(currentTimeString));
+if (currentTimeString) {
+    player.setCurrentTime(JSON.parse(currentTimeString));
+}
 
 player.on('timeupdate', throttle(function(data) {
     localStorage.setItem('videoplayer-current-time', data.seconds)
